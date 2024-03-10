2cbd48d64accd9fb58e539dd15f7cc911970820f
Eric Oliveira <ericjmoliveira@gmail.com>
2024-03-09 22:15:32 -0300

    feat: update app module imports list

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaService } from './prisma/prisma.service';
+import { AuthorsModule } from './authors/authors.module';
+import { GenresModule } from './genres/genres.module';
 
 @Module({
-  imports: [],
+  imports: [AuthorsModule, GenresModule],
   controllers: [AppController],
   providers: [AppService, PrismaService]
 })
